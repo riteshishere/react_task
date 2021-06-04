1da4303c803f8d863bc1d05011cfb27dbb592ea1
Ritesh Raj <34792135+riteshishere@users.noreply.github.com>
2021-06-04 22:10:07 +0530

    Signup component (#4)
    * Added route for register in Header
    
    * Added register reducer
    
    * Added register component

--- src/reducers/userSlice.js
+++ src/reducers/userSlice.js
@@ -7,6 +7,16 @@ const initialState = {
     error: null,
 }
 
+export const registerUser = createAsyncThunk('user/register', async ({ name, email, password, phone }) => {
+    const response = await API.post(
+        `/user/register`,
+        { name, email, password, phone },
+    )
+    console.log("Response from server is");
+    console.log(response)
+    return response.data
+})
+
 export const loginUser = createAsyncThunk('user/login', async ({ email, password }) => {
     const response = await API.post(
         `/user/login`,
@@ -46,6 +56,35 @@ const userSlice = createSlice({
             state.error = error.response && error.response.data.message
                 ? error.response.data.message
                 : error.message
+            state.loading = false
+        },
+        [registerUser.pending]: (state, action) => {
+            state.error = null
+            state.user = null
+            state.loading = true
+        },
+        [registerUser.fulfilled]: (state, action) => {
+            if (!action.payload.success) {
+                state.error = action.payload.msg
+                if (state.user !== null) state.user = null
+            } else {
+                state.error = null
+                state.user = action.payload
+            }
+            state.loading = false
+        },
+        [registerUser.rejected]: (state, action) => {
+            const error = action.payload
+            state.error = 
+                error.response
+                    ? error.response.data.message
+                        ? error.response.data.message
+                        : error.message
+                    :error.request
+                        ? error.request.data.message
+                            ? error.request.message
+                            : error.message
+                        : "Unknown error has been occured..."
             state.loading = false
         }
     }
