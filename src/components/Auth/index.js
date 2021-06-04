c84e06c2cb652871ad0a8b7cdabde68b05ed4dbc
ritesh raj <riteshraj26oct@gmail.com>
2021-06-05 00:55:47 +0530

    Added change-password component

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch, useRouteMatch } from 'react-router-dom'
 import Login from './Login'
 import Register from './Register'
+import ChangePassword from './ChangePassword'
 
 export const Auth = () => {
     let { path } = useRouteMatch();
@@ -10,6 +11,7 @@ export const Auth = () => {
         <Switch>
             <Route path={`${path}/login`} component={Login} />
             <Route path={`${path}/register`} component={Register} />
+            <Route path={`${path}/change-password`} component={ChangePassword} />
         </Switch>
     )
 }
