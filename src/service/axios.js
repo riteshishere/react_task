e8ccfbf5bba73dd7c144ed869c1dfd029ab8db2b
ritesh raj <riteshraj26oct@gmail.com>
2021-06-08 13:11:22 +0530

    included token in axios instance

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,5 +1,14 @@
-import axios from 'axios';
+import axios from 'axios'
+import isAuthenticUser from '../utilities/isAuthenticUser'
 
-export default axios.create({
-    baseURL: 'https://us-central1-ecomm-fed59.cloudfunctions.net/app'
+const baseURL = 'https://us-central1-ecomm-fed59.cloudfunctions.net/app'
+const token = isAuthenticUser()
+
+const axiosInstance = axios.create({
+    baseURL: baseURL
 })
+if (token) {
+    axiosInstance.defaults.headers.common['Authorization'] = `bearer ${token}`
+}
+
+export default axiosInstance
